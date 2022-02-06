5542ed7a72781603b0ded683e635e3a1577741ee
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-06 23:47:04 +0800

    Delete app directory

--- app/src/main/java/com/flavienlaurent/activityanimation/app/ThirdActivity.java
+++
@@ -1,9 +0,0 @@
-package com.flavienlaurent.activityanimation.app;
-
-public class ThirdActivity extends AnimatedRectActivity {
-
-    @Override
-    protected int layoutResId() {
-        return R.layout.activity_third;
-    }
-}
